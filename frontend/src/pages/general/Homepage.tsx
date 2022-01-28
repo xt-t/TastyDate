2ac7ef827e9fd074a051b4094eae77bfaa9f63f1
xt-t <xt.trinh@gmx.de>
2022-01-29 00:17:07 +0100

    implemented appmenu

---
+++ frontend/src/pages/general/Homepage.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../../components/general/NavigationBar";
+
+export default function Homepage() {
+
+    return (
+        <div>
+            <div>
+            <NavigationBar/>
+            </div>
+        </div>
+    )
+}
