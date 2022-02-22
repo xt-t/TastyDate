63cef8886aa1dc8c23d56facc9625901273977b3
xt-t <95277955+xt-t@users.noreply.github.com>
2022-02-22 15:52:02 +0100

    Update frontend/src/pages/result/VoteResult.tsx
    Co-authored-by: Flooooooooooorian <62606044+Flooooooooooorian@users.noreply.github.com>

--- frontend/src/pages/result/VoteResult.tsx
+++ frontend/src/pages/result/VoteResult.tsx
@@ -43,7 +43,7 @@ export default function VoteResult() {
         setVoteType(newValue);
     };
 
-    if (typeof tastyDateItemForVote !== "undefined") {
+    if (tastyDateItemForVote) {
     return (
         <div>
             <div>
