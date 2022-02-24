99f3c37487b8b97a505e31303facb75aca83fdd3
xt-t <xt.trinh@gmx.de>
2022-02-24 20:58:04 +0100

    Removing unnecessary import

--- frontend/src/components/appointmentsettings/AppointThree/AppointThree.tsx
+++ frontend/src/components/appointmentsettings/AppointThree/AppointThree.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import AppointThreeAddRestaurant from "./AppointThreeAddRestaurant";
-import AppointThreeDisplayRestaurant from "./AppointThreeDisplayRestaurant";
 import {AppointThreeType} from "../../../models/appointmentsettings/UseStateAppointStepTypes";
 import {useContext} from "react";
 import {AuthContext} from "../../../context/AuthProvider";
