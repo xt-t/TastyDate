443a0ce98fbc6d37c67d3697e67a46b13787c354
Xuan Thuy Trinh <xt.trinh@gmx.de>
2022-03-21 23:19:59 +0100

    added usersRestaurantList in RestaurantService

--- backend/src/main/java/de/neuefische/backend/service/RestaurantCardsService.java
+++ backend/src/main/java/de/neuefische/backend/service/RestaurantCardsService.java
@@ -1,6 +1,8 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.model.RestaurantCard;
+import de.neuefische.backend.model.loginregister.User;
+import de.neuefische.backend.repository.MongoUserRepository;
 import de.neuefische.backend.repository.RestaurantCardsRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +18,7 @@ import java.util.Optional;
 public class RestaurantCardsService {
 
     RestaurantCardsRepository restaurantCardList;
+    MongoUserRepository userRepository;
 
     public RestaurantCardsService(RestaurantCardsRepository restaurantCardList) {
         this.restaurantCardList = restaurantCardList;
@@ -24,6 +28,20 @@ public class RestaurantCardsService {
         return restaurantCardList.findAll();
     }
 
+    public List<RestaurantCard> getUsersRestaurantCards(Principal principal) {
+        String currentUser = principal.getName();
+        Optional<User> user = userRepository.findByUsername(currentUser);
+        List<RestaurantCard> userRestauarantList = new ArrayList<RestaurantCard>();
+        if (user.isPresent()) {
+            List<String> userIdsRestaurants = user.get().getFavouriteRestaurantsIds();
+            for (String userIdsRestaurant:userIdsRestaurants) {
+                Optional<RestaurantCard> tempRestaurant = findRestaurantCardById(userIdsRestaurant);
+                tempRestaurant.ifPresent(userRestauarantList::add);
+            }
+        }
+        return userRestauarantList;
+    }
+
     public Optional<RestaurantCard> findRestaurantCardById(String id) {
         return restaurantCardList.findById(id);
     }
@@ -31,6 +49,13 @@ public class RestaurantCardsService {
     public RestaurantCard addNewRestaurant(RestaurantCard restaurantCard, Principal principal) {
         String cardCreatorName = principal.getName();
         restaurantCard.setCardCreator(cardCreatorName);
+        Optional<User> user = userRepository.findByUsername(cardCreatorName);
+        if (user.isPresent()) {
+            List<String> tempRestaurantsListUser = user.get().getFavouriteRestaurantsIds();
+            tempRestaurantsListUser.add(restaurantCard.getId());
+            user.get().setFavouriteRestaurantsIds(tempRestaurantsListUser);
+            userRepository.save(user.get());
+        }
         return restaurantCardList.insert(restaurantCard);
     }
 
